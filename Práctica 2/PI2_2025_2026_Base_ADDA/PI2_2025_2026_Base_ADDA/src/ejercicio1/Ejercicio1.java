63b6f3ef8e6bebc16c170004aae882ca33c9a8c0
mibullfire <mibullfire@gmail.com>
2025-11-15 10:47:24 +0100

    Ejercicios 1 y 2

--- Práctica 2/PI2_2025_2026_Base_ADDA/PI2_2025_2026_Base_ADDA/src/ejercicio1/Ejercicio1.java
+++ Práctica 2/PI2_2025_2026_Base_ADDA/PI2_2025_2026_Base_ADDA/src/ejercicio1/Ejercicio1.java
@@ -8,6 +8,9 @@ import us.lsi.tiposrecursivos.BinaryTree;
 import us.lsi.tiposrecursivos.BEmpty;
 import us.lsi.tiposrecursivos.BLeaf;
 import us.lsi.tiposrecursivos.BTree;
+import us.lsi.tiposrecursivos.TEmpty;
+import us.lsi.tiposrecursivos.TLeaf;
+import us.lsi.tiposrecursivos.TNary;
 import us.lsi.tiposrecursivos.Tree;
 
 public class Ejercicio1 {	
@@ -15,39 +18,82 @@ public class Ejercicio1 {
 	// Pair<List<Integer>, Integer> Donde la lista es el camino que llevo y el entero es su multiplicación
 	
 	public static List<Integer> caminoMaximo (BinaryTree<Integer> tree) {
-		Pair<List<Integer>, Integer> par = Pair.of(new ArrayList<Integer>(), 1);
-		return caminoMaximo(tree, par, par, 0).first();
+		Pair<List<Integer>, Integer> pairBase = Pair.of(new ArrayList<Integer>(), 1);
+		return caminoMaximo(tree, pairBase, pairBase).first();
 	}
 	
-	private static Pair<List<Integer>, Integer> caminoMaximo(BinaryTree<Integer> tree, Pair<List<Integer>, Integer> par, Pair<List<Integer>, Integer> maximo
-, Integer i) {
+	private static Pair<List<Integer>, Integer> caminoMaximo(BinaryTree<Integer> tree, Pair<List<Integer>, Integer> par, Pair<List<Integer>, Integer> max) {
+		
+		/* "aux" es un par auxiliar, que voy a usar para el caso de las hojas. El objetivo de "aux" es almacenar un par auxiliar, que podría ser candidato 
+		 * para convertirse en el nuevo "max".
+		 */
+		
 		Pair<List<Integer>, Integer> aux = null;
+		
 		switch (tree) {
-		case BEmpty():
-			break;
-		case BLeaf(var lb): 
-			List<Integer> lista = new ArrayList<Integer>(par.first());
-			lista.add(lb);
-			aux = Pair.of(lista, par.second()*lb);
-			if (aux.second() > maximo.second()) {
-				maximo = aux;
+			case BEmpty<Integer> t -> {;}
+			case BLeaf<Integer> t -> {
+				
+				List<Integer> lista = par.first();
+				lista.add(t.label());
+				aux = Pair.of(lista, par.second()*t.label());
+				if (aux.second() > max.second()) max = aux;
+				
 			}
-			break;
-		case BTree(var lb, var lt, var rt):
-			List<Integer> lista2i = new ArrayList<Integer>(par.first());
-			lista2i.add(lb);
-			maximo = caminoMaximo(lt, Pair.of(lista2i, par.second()*lb), maximo, i+1);
-			List<Integer> lista2d = new ArrayList<Integer>(par.first());
-			lista2d.add(lb);
-			maximo = caminoMaximo(rt, Pair.of(lista2d, par.second()*lb), maximo, i+1);
-			break;
+			case BTree<Integer> t -> {
+				
+				/* Para afrontar este problema, he creado dos listas auxiliares, una para la rama izquierda y otra para la derecha, cada una llamará recursivamente
+				 * a la función, hasta llegar a una hoja, donde resolveremos el problema.
+				 */
+				
+				List<Integer> lista2i = new ArrayList<Integer>(par.first());
+				lista2i.add(t.label());
+				max = caminoMaximo(t.left(), Pair.of(lista2i, par.second()*t.label()), max);
+				List<Integer> lista2d = new ArrayList<Integer>(par.first());
+				lista2d.add(t.label());
+				max = caminoMaximo(t.right(), Pair.of(lista2d, par.second()*t.label()), max);
+				
+			}
 		}
 		
-		return maximo;
+		return max;
 	}
 
 	public static List<Integer> caminoMaximo (Tree<Integer> tree) {
-		return null;
+		Pair<List<Integer>, Integer> pairBase = Pair.of(new ArrayList<Integer>(), 1);
+		return caminoMaximo(tree, pairBase, pairBase, 0).first();
+	}
+	
+	private static Pair<List<Integer>, Integer> caminoMaximo(Tree<Integer> tree, Pair<List<Integer>, Integer> par, Pair<List<Integer>, Integer> max, Integer i) {
+		
+		/* "aux" es un par auxiliar, que voy a usar para el caso de las hojas. El objetivo de "aux" es almacenar un par auxiliar, que podría ser candidato 
+		 * para convertirse en el nuevo "max".
+		 */
+		
+		Pair<List<Integer>, Integer> aux = null;
+		
+		switch (tree) {
+			case TEmpty<Integer> t -> {;}
+			case TLeaf<Integer> t -> {
+				
+				List<Integer> lista = par.first();
+				lista.add(t.label());
+				aux = Pair.of(lista, par.second()*t.label());
+				if (aux.second() > max.second()) max = aux;
+				
+			}
+			case TNary<Integer> t -> {
+				
+				for (Tree<Integer> tt : t.children()) {
+					List<Integer> auxChd = new ArrayList<Integer>(par.first());
+					auxChd.add(t.label());
+					max = caminoMaximo(tt, Pair.of(auxChd, par.second()*t.label()), max, i+1);
+				}
+			}
+		}
+		
+		return max;
+		
 	}
 	
 }
--- Práctica 2/PI2_2025_2026_Base_ADDA/PI2_2025_2026_Base_ADDA/src/ejercicio2/Ejercicio2.java
+++ Práctica 2/PI2_2025_2026_Base_ADDA/PI2_2025_2026_Base_ADDA/src/ejercicio2/Ejercicio2.java
@@ -1,6 +1,16 @@
 package ejercicio2;
 
 import us.lsi.tiposrecursivos.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import us.lsi.tiposrecursivos.BEmpty;
+import us.lsi.tiposrecursivos.BLeaf;
+import us.lsi.tiposrecursivos.BTree;
+import us.lsi.tiposrecursivos.TEmpty;
+import us.lsi.tiposrecursivos.TLeaf;
+import us.lsi.tiposrecursivos.TNary;
 import us.lsi.tiposrecursivos.Tree;
 
 
@@ -8,11 +18,103 @@ import us.lsi.tiposrecursivos.Tree;
 public class Ejercicio2 {
 	
 	public static Boolean solucion_recursiva(BinaryTree<String> tree) {
-		return null;
+		return solucion_recursiva(tree, 0, null).ok;
+	}
+	
+	private static Res solucion_recursiva(BinaryTree<String> tree, Integer cuenta, Integer objetivo) {
+				
+		switch(tree) {
+			
+			case BEmpty<String> t -> {
+				return new Res(true, 0);
+			}
+			case BLeaf<String> t -> {
+				int total = cuenta + numVc(t.label());
+				if (objetivo==null)
+					objetivo = total;
+				 return new Res(objetivo==total, numVc(t.label()));
+							
+			}
+			case BTree<String> t -> {
+				
+				int vocalesNodo = numVc(t.label());
+	
+		        // subárbol izquierdo
+		        Res izq = solucion_recursiva(t.left(), cuenta + vocalesNodo, objetivo);
+	
+		        // subárbol derecho
+		        Res der = solucion_recursiva(t.right(), cuenta + vocalesNodo, objetivo);
+	
+		        // condición a comprobar en este nodo
+		        boolean ok = izq.ok && der.ok && (izq.vocales == der.vocales);
+	
+		        return new Res(ok, izq.vocales + der.vocales + vocalesNodo);
+				
+				
+			}
+		}
+		
 	}
 
 	public static Boolean solucion_recursiva(Tree<String> tree) {
-		return null;
+		return solucion_recursiva(tree, 0, null).ok;
+	}
+	
+	private static Res solucion_recursiva(Tree<String> tree, Integer cuenta, Integer objetivo) {
+		
+		switch(tree) {
+		case TEmpty<String> t -> {
+			return new Res(true, 0);
+		}
+		case TLeaf<String> t -> {
+			int total = cuenta + numVc(t.label());
+			if (objetivo==null) objetivo = total;
+			return new Res(objetivo==total, numVc(t.label()));
+		}
+		case TNary<String> t -> {
+			
+			//// NOTA: todo este trozo es una chatgepeteada
+			
+			int vocalesNodo = numVc(t.label());
+			
+		    // Procesar todos los hijos
+		    List<Res> resultados = new ArrayList<>();
+		    for (Tree<String> hijo : t.children()) {
+		        Res resHijo = solucion_recursiva(hijo, cuenta + vocalesNodo, objetivo);
+		        resultados.add(resHijo);
+		    }
+		    
+		    // Verificar que todos los hijos tengan el mismo número de vocales
+		    boolean todosOk = true;
+		    int vocalesPrimerHijo = resultados.get(0).vocales;
+		    int totalVocalesHijos = 0;
+		    
+		    for (Res res : resultados) {
+		        todosOk = todosOk && res.ok && (res.vocales == vocalesPrimerHijo);
+		        totalVocalesHijos += res.vocales;
+		    }
+		    
+		    return new Res(todosOk, totalVocalesHijos + vocalesNodo);
+			
+		}
+		
+		}
+	}
+	
+	private record Res(Boolean ok, Integer vocales) {}
+	
+	private static Integer numVc(String palabra) {
+		int count = 0;
+		palabra = palabra.toLowerCase();  // Para evitar problemas con mayúsculas
+
+        for (int i = 0; i < palabra.length(); i++) {
+            char c = palabra.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        
+        return count;
 	}
 	
 }
