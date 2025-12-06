dbdeb9e85f347edf0c0fa2d03e8669d0acc8a45b
mibullfire <mibullfire@gmail.com>
2025-12-06 16:25:18 +0100

    P2 Cuasi Terminada

--- Práctica 2/PI2_2025_2026_Base_ADDA/PI2_2025_2026_Base_ADDA/src/ejercicio4/Ejercicio4.java
+++ Práctica 2/PI2_2025_2026_Base_ADDA/PI2_2025_2026_Base_ADDA/src/ejercicio4/Ejercicio4.java
@@ -6,8 +6,10 @@ import java.util.Set;
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.alg.tour.HeldKarpTSP;
 
 import us.lsi.colors.GraphColors;
+import us.lsi.colors.GraphColors.Color;
 import us.lsi.colors.GraphColors.Style;
 import us.lsi.grafos.datos.Carretera;
 import us.lsi.grafos.datos.Ciudad;
@@ -17,25 +19,36 @@ public class Ejercicio4 {
 
 	public static GraphPath<Interseccion,Calle> getSubgraph_EJ4A(String mIn, String mOut,Graph <Interseccion, Calle> g, String ftest) {
 		var alg = new DijkstraShortestPath<>(g);
-		Interseccion origen = Interseccion.ofId(Integer.valueOf(mIn));
-		Interseccion destino = Interseccion.ofId(Integer.valueOf(mOut));
-		System.out.println(origen);
-		System.out.println(g.vertexSet());
+		Interseccion origen = g.vertexSet().stream().filter(x->x.getNombre().equals(mIn)).findFirst().get();
+		Interseccion destino = g.vertexSet().stream().filter(x->x.getNombre().equals(mOut)).findFirst().get();
 		
-		GraphPath<Interseccion, Calle> gp = alg.getPath(origen, destino);
+		// Cuando ya tenemos los dos vértices aplicamos el algoritmo de Dijkstra:
+		GraphPath<Interseccion, Calle> res = DijkstraShortestPath.findPathBetween(g, origen, destino);
 		
-		GraphColors.toDot(g,"ficheros_generados/" + ftest + "A.gv",
+		GraphColors.toDot(g,ftest,
 				x->x.getNombre(), x->""+x.getId(),
-				v->GraphColors.styleIf(Style.bold, gp.getVertexList().contains(v)),
-				e->GraphColors.styleIf(Style.bold, gp.getEdgeList().contains(e)));
+				v->GraphColors.colorIf(Color.blue, Color.black, res.getVertexList().contains(v)),
+				e->GraphColors.colorIf(Color.blue, Color.black, res.getEdgeList().contains(e)));
 		
-		System.out.println(ftest + "A.gv generado en " + "ficheros_generados/p2/ejemplo4");
+		System.out.println(ftest + " generado!");
 		
-		return null;
+		return res;
 	}
 	
 	public static GraphPath<Interseccion,Calle> getRecorrido_E4B(Graph <Interseccion, Calle> g) {
-        return null;
+		HeldKarpTSP<Interseccion, Calle> TSP = new HeldKarpTSP<Interseccion, Calle>();
+		GraphPath<Interseccion, Calle> res = TSP.getTour(g); 
+		
+		GraphColors.toDot(g, "ficheros_generados/EJ4ApartadoB.gv",
+				v->v.getNombre(), 
+				e->"" + e.getEsfuerzo(), 
+				v->GraphColors.colorIf(Color.blue, Color.black, res.getVertexList().contains(v)), 
+				e->GraphColors.colorIf(Color.blue, Color.black, res.getEdgeList().contains(e))
+				);
+		
+		System.out.println("ficheros_generados/EJ4ApartadoB.gv" + " generado!");
+		
+		return res; 
 	}
 	
 	public static Graph<Interseccion,Calle> getRecorridoMaxRelevante_E4C(Set<Calle> cs,Graph <Interseccion, Calle> g, String ftest) {
--- Práctica 2/PI2_2025_2026_Base_ADDA/PI2_2025_2026_Base_ADDA/src/tests/TestEjercicio4.java
+++ Práctica 2/PI2_2025_2026_Base_ADDA/PI2_2025_2026_Base_ADDA/src/tests/TestEjercicio4.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.jgrapht.Graph;
 import org.jgrapht.graph.SimpleWeightedGraph;
 
 import ejercicio4.Calle;
@@ -22,27 +23,43 @@ public class TestEjercicio4 {
 	}
 	
 	private static void test(String fichero) {
-		SimpleWeightedGraph<Interseccion,Calle> g =  
+		
+		
+		Graph<Interseccion,Calle> gDuracion =  
 				GraphsReader.newGraph("ficheros/" + fichero + ".txt",
 						Interseccion::ofFormat, 
 						Calle::ofFormat,
-						Graphs2::simpleWeightedGraph);
-
+						Graphs2::simpleWeightedGraph,
+						Calle::getDuracion);
+		
+		Graph<Interseccion,Calle> gEsfuerzo =  
+				GraphsReader.newGraph("ficheros/" + fichero + ".txt",
+						Interseccion::ofFormat, 
+						Calle::ofFormat,
+						Graphs2::simpleWeightedGraph,
+						Calle::getEsfuerzo);
+		
+		/// PRUEBAS
+		/*
 		System.out.println("Archivo de entrada " + fichero + ".txt \n" + "Datos de entrada: " + g);
 
 		GraphColors.toDot(g,"ficheros_generados/" + fichero + ".gv",
-				//v->v.hasMonumento()?String.valueOf("INT-" + v.getId() + ", m-"+ v.getRelevancia()):String.valueOf("INT-" + v.getId()), //que etiqueta mostrar en vertices y aristas
+				v->v.hasMonumento()?String.valueOf("INT-" + v.getId() + ", m-"+ v.getRelevancia()):String.valueOf("INT-" + v.getId()), //que etiqueta mostrar en vertices y aristas
 				v->""+v.getId(),
 				//e->String.valueOf("Duracion: " + (int) e.getDuracion() + " - Esfuerzo: " + (int) e.getEsfuerzo()),
 				e->""+e.getDuracion(),
 				v->GraphColors.color(Color.black), //color o estilo de vertices y aristas
 				e->GraphColors.color(Color.black));
+		*/
+		///
 		
+		/// === Apartado A === ///
 		System.out.println("Apartado A):");
-		Ejercicio4.getSubgraph_EJ4A("2", "5", g, "");
+		//Ejercicio4.getSubgraph_EJ4A("m7", "m3", gDuracion, "ficheros_generados/EJ4ApartadoA1.gv"); Da error, porque no existe m3
+		Ejercicio4.getSubgraph_EJ4A("m4", "m9", gDuracion, "ficheros_generados/EJ4ApartadoA2.gv");
 		
 		System.out.println("Apartado B):");
-		//Ejemplo4.apartadoB(g, fichero);
+		Ejercicio4.getRecorrido_E4B(gEsfuerzo);
 	}
 	
 
